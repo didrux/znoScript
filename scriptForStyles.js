21cbd1639dabdf7573ff36e483f6d54f5354575e
didrux <60069950+didrux@users.noreply.github.com>
2020-01-19 21:02:20 +0200

    Added script for styles

---
+++ scriptForStyles.js
@@ -0,0 +1,4 @@
+let commentsToTheTasksArray = document.getElementsByClassName('explanation');
+for (let i = 0; i < commentsToTheTasksArray.length; i++){
+	commentsToTheTasksArray[i].style.display = 'inline-block';
+}
